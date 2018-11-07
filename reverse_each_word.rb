231232127c0b5fd3677b0e27f853598e3e1fb7ec
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-07 14:58:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
-def reverse_each_word(str)
-  s.gsub(/\w+/) { |match| match.reverse }
+def reverse_each_word(string)
+  array = string.split
+  array.collect { |word| word.reverse}
+  array.join(" ")
 end
+
+
+

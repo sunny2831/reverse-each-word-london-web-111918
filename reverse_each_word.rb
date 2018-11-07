3d82b4c52ff0803f673ad3cc4ee5158ac18c29cf
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-07 15:15:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split
-  reversed_string = array.each do |word|
-    word.reverse!
+  reversed_string = array.each { |word|
+    word.reverse! }
     end
   return reversed_string.join(" ")
 end
